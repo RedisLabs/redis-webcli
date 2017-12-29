104def3c7cf1df008cef502fc1dc41623916b835
Yossi Gottlieb <yossigo@gmail.com>
2017-12-29 19:41:52 +0000

    Cloud Foundry integration.

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
+import os
+import json
+import urllib
 from flask import Flask, render_template, request, jsonify
 from flask_redis import FlaskRedis
+from flask_redis_sentinel import SentinelExtension
 from flask_bootstrap import Bootstrap
 
+redis_sentinel = SentinelExtension()
+redis_db = redis_sentinel.default_connection
+
 app = Flask(__name__)
-app.config.from_envvar('APP_SETTINGS')
+redis_password = None
 
+# Handle Cloud Foundry with Sentinel
+if 'VCAP_SERVICES' in os.environ:
+  services = json.loads(os.getenv('VCAP_SERVICES'))
+  service = services.get('redislabs')[0]
+  creds = service['credentials']
+  redis_password = creds['password']
+  app.config['REDIS_URL'] = 'redis+sentinel://:%s@%s:%s/%s/0' % (
+    urllib.quote(creds['password'], safe=''),
+    creds['sentinel_addrs'][0],
+    creds['sentinel_port'],
+    urllib.quote(creds['name'], safe=''))
+
+
+redis_sentinel.init_app(app)
 Bootstrap(app)
-
-redis_db = FlaskRedis(app)
 
 @app.route('/execute', methods=['POST'])
 def execute():
