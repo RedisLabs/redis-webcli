d42340a33385d72e7916392f2d603174526a4ce9
Shaya Potter <shaya@redislabs.com>
2019-12-25 10:14:17 +0200

    RED-35574: bytestring to string conversion for python3

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ class MemtierThread(threading.Thread):
         self._process = subprocess.Popen(["./memtier_benchmark", "-s", self._master_ip, "-p", self._master_port, "-a", self._redis_password] + self._argument_list,
                                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, shell=False)
         while True:
-            curr_output = self._process.stdout.readline()
+            curr_output = self._process.stdout.readline().decode("utf-8")
             if "[RUN" in curr_output:
                 temp_output = curr_output.split("[RUN")
                 curr_output = "\n[RUN".join(temp_output)
